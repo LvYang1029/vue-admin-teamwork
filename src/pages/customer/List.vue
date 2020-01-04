93b8ec4c7963c5ab143475799007f305ca847d4d
yuanshuai0407 <894989495@qq.com>
2020-01-04 10:30:14 +0800

    customer3

--- src/pages/customer/List.vue
+++ src/pages/customer/List.vue
@@ -51,3 +51,104 @@
 
   </div>
 </template>
+<script>
+import request from '@/utils/request'
+import querystring from 'querystring'
+export default {
+  // 用于存放网页中需要调用的方法
+  methods:{
+    loadData(){
+      let url = "http://localhost:6677/customer/findAll"
+      request.get(url).then((response)=>{
+        // 将查询结果设置到customers中，this指向外部函数的this
+        this.customers = response.data;
+      })
+    },
+    submitHandler(){
+      //this.form 对象 ---字符串--> 后台 {type:'customer',age:12}
+      // json字符串 '{"type":"customer","age":12}'
+      // request.post(url,this.form)
+      // 查询字符串 type=customer&age=12
+      // 通过request与后台进行交互，并且要携带参数
+      let url = "http://localhost:6677/customer/saveOrUpdate";
+      request({
+        url,
+        method:"POST",
+        headers:{
+          "Content-Type":"application/x-www-form-urlencoded"
+        },
+        data:querystring.stringify(this.form)
+      }).then((response)=>{
+        // 模态框关闭
+        this.closeModalHandler();
+        // 刷新
+        this.loadData();
+        // 提示消息
+        this.$message({
+          type:"success",
+          message:response.message
+        })
+      })
+
+    },
+    toDeleteHandler(id){
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+
+        let url = "hhttp://locaahost:6677/customer/deleteById?id="+id;
+        request.get(url).then((response)=>{
+
+          this.loadData();
+
+
+
+
+
+
+          this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+
+        })
+
+
+        
+      })
+      
+    },
+    toUpdateHandler(){
+      this.form = row;
+      this.visible = true;
+    },
+    closeModalHandler(){ 
+      this.visible = false;
+    },
+    toAddHandler(){
+      this.form={
+        type:"customer"
+      }
+      this.visible = true;
+    }
+  },
+  // 用于存放要向网页中显示的数据
+  data(){
+    return {
+      visible:false,
+      customers:[],
+      form:{
+        type:"customer"
+      }
+    }
+  },
+  created(){
+    // this为当前vue实例对象
+    // vue实例创建完毕 
+    this.loadData()
+
+  }
+}
+</script>
